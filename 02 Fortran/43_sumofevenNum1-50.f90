fc875239251879288fb6be43d71b73bc75f5c89c
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-07-07 06:46:39 +0545

    exam question 2014 fall

---
+++ 02 Fortran/43_sumofevenNum1-50.f90
@@ -0,0 +1,11 @@
+program sumofevennumberfrom1to50
+implicit none
+integer :: indexer, sum
+
+do indexer = 1, 50
+    sum = sum + indexer
+    end do
+
+print *, "Sum of the number is ", sum
+
+end program sumofevennumberfrom1to50
